59defbdd2b532f378696c5819034c36047e3f90e
unknown <r.puggaard@phonetik.uni-muenchen.de>
2024-01-05 19:10:15 +0100

    bitmap raster plotting of spectrograms

--- R/specplot.R
+++ R/specplot.R
@@ -24,6 +24,11 @@
 #' @param windowShape String giving the name of the window shape to be applied
 #' to the signal when generating spectrograms. Default is `Gaussian`; other
 #' options are `square`, `Hamming`, `Bartlett`, or `Hanning`.
+#' @param colors Vector of strings giving the names of colors to be used
+#' for plotting the spectrogram; default is `c('white', 'black')`. The first
+#' value is used for plotting the lowest visible amplitude, and the last for
+#' plotting the highest visible amplitude. Vectors with more than two color
+#' names can be used for plotting values in between in different colors.
 #' @param formant_plotOnSpec Boolean; should formants be plotted on top of
 #' spectrogram? Default is `FALSE`.
 #' @param fm Formant object loaded using [rPraat::formant.read]
@@ -34,6 +39,10 @@
 #' When a formant plot of `formant_plotType='speckle'` is drawn, no formants are
 #' shown in frames with intensity level `formant_dynamicRange` below the maximum
 #' intensity. Default is `30`. If set to `0`, all formants are shown.
+#' @param formant_color String or vector of strings giving the name(s) of
+#' colors to be used for plotting formants. If one color is provided, all
+#' formants will be plotted in this color. If multiple colors are provided,
+#' different formants will be shown in different colors. Default is `'black'`.
 #' @param tgbool Logical; should dotted lines be plotted corresponding to
 #' locations in a TextGrid? Default is `FALSE`.
 #' @param lines Numeric vector giving locations in seconds of locations from
@@ -58,8 +67,10 @@
 #' praatpicture(paste0(datapath, '/1.wav'), frames='spectrogram')
 specplot <- function(sig, sr, t, start, end, tfrom0=TRUE, freqRange=c(0,5000),
                      windowLength=0.005, dynamicRange=60, timeStep=1000,
-                     windowShape='Gaussian', formant_plotOnSpec=FALSE, fm=NULL,
+                     windowShape='Gaussian', colors=c('white', 'black'),
+                     formant_plotOnSpec=FALSE, fm=NULL,
                      formant_plotType='draw', formant_dynamicRange=30,
+                     formant_color='black',
                      tgbool=FALSE, lines=NULL, ind=NULL, nframe=NULL,
                      start_end_only=TRUE, min_max_only=TRUE,
                      axisLabel='Frequency (Hz)') {
@@ -112,9 +123,19 @@ specplot <- function(sig, sr, t, start, end, tfrom0=TRUE, freqRange=c(0,5000),
                                  windowlength=wl, window=ws,
                                  dynamicrange=dynamicRange)
 
+  freqdom <- as.numeric(unlist(dimnames(spec$spectrogram)[2]))
+  within_freqran <- which(freqdom < freqRange[2])
+  spec$spectrogram <- spec$spectrogram[,within_freqran]
+
   time_s <- as.numeric(rownames(spec$spectrogram))/1000
   if (!tfrom0) time_s <- time_s + start
   rownames(spec$spectrogram) <- time_s
+
+  if (grDevices::dev.capabilities()$rasterImage == 'yes') {
+    useRaster <- TRUE
+  } else {
+    useRaster <- FALSE
+  }
 
   plot(NULL, xaxt=xax, xlim=c(start, end+start),
        ylim=freqRange, yaxs='i', yaxt=yax)
@@ -124,11 +145,27 @@ specplot <- function(sig, sr, t, start, end, tfrom0=TRUE, freqRange=c(0,5000),
                                         tick=F)
   graphics::mtext(axisLabel, side=2, line=3.5, cex=0.8)
 
-  plot(spec, add=T)
+  fillCol <- grDevices::colorRampPalette(colors)
+  fillRan <- c(-dynamicRange, 0)
+  nlevels <- abs(-dynamicRange - 0) * 1.2
+  levels <- pretty(fillRan, nlevels)
+  fillCol <- fillCol(length(levels) - 1)
+  subdr <- which(spec$spectrogram < -dynamicRange)
+  spec$spectrogram[subdr] <- -dynamicRange
+
+  specDims <- dim(spec$spectrogram)
+  xVals <- seq(min(time_s), max(time_s), length.out=specDims[1])
+  yVals <- seq(freqRange[1], freqRange[2], length.out=specDims[2])
+
+  graphics::image(x=xVals, y=yVals, z=spec$spectrogram,
+        col=fillCol, useRaster=TRUE, add=TRUE)
+  graphics::box()
+
   if (tgbool) graphics::abline(v=lines, lty='dotted')
 
   if (formant_plotOnSpec) {
     nf <- fm$maxnFormants
+    if (length(formant_color) == 1) formant_color <- rep(formant_color, nf)
     if (tfrom0) fm$t <- fm$t - org_start
     if (fm$conv2db) {
       db <- gsignal::pow2db(fm$intensityVector)
@@ -142,15 +179,17 @@ specplot <- function(sig, sr, t, start, end, tfrom0=TRUE, freqRange=c(0,5000),
       subdr <- 1
     }
     if (formant_plotType == 'draw') {
-      graphics::lines(fm$t, fm$frequencyArray[1,])
+      graphics::lines(fm$t, fm$frequencyArray[1,], col=formant_color[1])
       for (i in 2:nf) {
-        graphics::lines(fm$t, fm$frequencyArray[i,])
+        graphics::lines(fm$t, fm$frequencyArray[i,], col=formant_color[i])
       }
     }
     if (formant_plotType == 'speckle') {
-      graphics::points(fm$t[-subdr], fm$frequencyArray[1,-subdr], pch=20)
+      graphics::points(fm$t[-subdr], fm$frequencyArray[1,-subdr], pch=20,
+                       col=formant_color[1])
       for (i in 2:nf) {
-        graphics::points(fm$t[-subdr], fm$frequencyArray[i,-subdr], pch=20)
+        graphics::points(fm$t[-subdr], fm$frequencyArray[i,-subdr], pch=20,
+                         col=formant_color[i])
       }
     }
   }
